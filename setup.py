6fe8cf1f6b68186e7ec2c4090c35fd9c2957d382
naeemkh <khoshnevis.naeem@gmail.com>
2021-04-18 22:17:21 -0500

    added licence

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="TBD",
-    license="TBD",
+    license="GPL-3",
     packages=find_packages(exclude=['docs*', 'tests*']),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
