d5596d2daf13b298e2d501a0c943028178eb3ada
naeemkh <khoshnevis.naeem@gmail.com>
2021-05-16 15:11:42 -0500

    initilize files.

---
+++ pycausalgps/dataset.py
@@ -0,0 +1,28 @@
+
+
+
+class Dataset:
+
+
+    def __init__(self) -> None:
+        pass
+
+
+    def read_from_disk(self):
+        pass
+
+
+    def write_to_disk(self):
+        pass
+
+
+    def read_from_database(self):
+        pass
+
+
+    def write_to_database(self):
+        pass
+
+
+    def compute_covariate_balance(self):
+        pass
---
+++ pycausalgps/population.py
@@ -0,0 +1,36 @@
+from .dataset import Dataset
+
+class Population(Dataset):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+    def estimate_gps(self):
+        pass
+
+    def generate_pseudo_pop(self):
+        pass
+    
+    def compile_pseudo_pop(self):
+        pass
+
+    def matching_l1(self):
+        pass
+
+    def train_it(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def create_matching(self):
+        pass
+
+    def create_weighting(self):
+        pass
+
+    def generate_synthetic_data(self):
+        pass
+
