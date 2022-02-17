139c5b325246fca87aae1a597861c2a36fcadce7
naeemkh <khoshnevis.naeem@gmail.com>
2022-02-17 09:15:55 -0500

    having progress ...

--- pycausalgps/project.py
+++
@@ -1,8 +0,0 @@
-
-
-class Project:
-    """ Project Class
-
-    """
-    pass
-
---
+++ pycausalgps/project_utils.py
@@ -0,0 +1,25 @@
+"""
+project_utils.py
+=======================================
+Utility functions to store and retireve functions into/from database.
+"""
+
+
+def projects_list(db):
+    pass
+
+
+def save_project(db, pr_name):
+    pass
+
+
+def load_project(db, pr_name):
+    pass
+
+
+
+
+
+
+
+
