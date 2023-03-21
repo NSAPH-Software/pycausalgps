ea0cb379e281c8fd888b9d900b658aef51512f29
naeemkh <khoshnevis.naeem@gmail.com>
2023-03-20 23:34:38 -0400

    add document test.

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -7,16 +7,16 @@ import pycausalgps.gps as gps
 import pycausalgps.project_controller as project_controller
 
 
-# def test_doctest_suit():
-#     test_suit = unittest.TestSuite()
+def test_doctest_suit():
+    test_suit = unittest.TestSuite()
 
-#     # add tests
-#     #test_suit.addTest(doctest.DocTestSuite(gps))
-#     #test_suit.addTest(doctest.DocTestSuite(gps_utils))
-#     #test_suit.addTest(doctest.DocTestSuite(database))
-#     #test_suit.addTest(doctest.DocTestSuite(project_controller))
+    # add tests
+    test_suit.addTest(doctest.DocTestSuite(gps))
+    # test_suit.addTest(doctest.DocTestSuite(gps_utils))
+    test_suit.addTest(doctest.DocTestSuite(database))
+    test_suit.addTest(doctest.DocTestSuite(project_controller))
     
-#     # set runner
-#     runner = unittest.TextTestRunner(verbosity=2).run(test_suit)
+    # set runner
+    runner = unittest.TextTestRunner(verbosity=2).run(test_suit)
 
-#     assert not runner.failures
+    assert not runner.failures
